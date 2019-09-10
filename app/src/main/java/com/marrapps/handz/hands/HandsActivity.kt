2e251e2b491a8966ceabdd282f01228ba1213506
mnetoae <rafaelmarraneto@correio.itaucom.br>
2019-09-10 14:05:49 -0300

    arquivos faltantes

---
+++ app/src/main/java/com/marrapps/handz/hands/HandsActivity.kt
@@ -0,0 +1,47 @@
+package com.marrapps.handz.hands
+
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.marrapps.handz.BaseActivity
+import com.marrapps.handz.R
+import com.marrapps.handz.model.numbers.NumbersResponse
+import kotlinx.android.synthetic.main.activity_list.*
+import kotlinx.android.synthetic.main.content_list.*
+
+class HandsActivity : BaseActivity(), HandsContract.View {
+
+    private lateinit var presenter: HandsContract.Presenter
+    private lateinit var numbersResponse: NumbersResponse
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list)
+        setSupportActionBar(toolbar)
+
+        setupRecyclerView()
+
+        numbersResponse = intent.extras?.getSerializable("numbers") as NumbersResponse
+
+        presenter = HandsPresenter(this)
+        presenter.fetchImages(numbersResponse)
+    }
+
+    private fun setupRecyclerView() {
+        rvHandsList.apply {
+            setHasFixedSize(true)
+            layoutManager = LinearLayoutManager(this@HandsActivity)
+            adapter = HandsRecyclerAdapter(emptyList())
+            setHasFixedSize(true)
+        }
+    }
+
+    override fun fetchComplete(urlList: List<String>) {
+        rvHandsList.adapter = HandsRecyclerAdapter(urlList)
+    }
+
+    override fun onError() {
+        showErrorDialog(
+            "Não foi possível carregar as informações. Por favor, tente novamente mais tarde."
+        ) { presenter.fetchImages(numbersResponse) }
+    }
+}
---
+++ app/src/main/java/com/marrapps/handz/hands/HandsRecyclerAdapter.kt
@@ -0,0 +1,41 @@
+package com.marrapps.handz.hands
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.marrapps.handz.R
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.list_hands_item.view.*
+
+class HandsRecyclerAdapter(private val urlList: List<String>) :
+    RecyclerView.Adapter<HandsRecyclerAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): ViewHolder {
+
+        val view: View =
+            LayoutInflater.from(p0.context).inflate(R.layout.list_hands_item, p0, false)
+
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(p0: ViewHolder, p1: Int) {
+        p0.bind(urlList[p1], p1 + 1)
+    }
+
+    override fun getItemCount() = urlList.size
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        fun bind(url: String, position: Int) {
+
+            itemView.tvHandNumber.text = position.toString()
+
+            Picasso.get()
+                .load(url)
+                .placeholder(R.drawable.handz)
+                .error(R.drawable.handz)
+                .into(itemView.ivHandImage)
+        }
+    }
+}
